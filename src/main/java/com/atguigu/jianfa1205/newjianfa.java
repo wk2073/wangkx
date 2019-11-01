cc1bb20bb3c30dbf98584aa1d1d7e6ff5f3c47ad
wangkun <765505687@qq.com>
2019-11-01 19:45:18 +0800

    new jianfa

---
+++ src/main/java/com/atguigu/jianfa1205/newjianfa.java
@@ -0,0 +1,12 @@
+package com.atguigu.jianfa1205;
+
+/**
+ * @author by wangkun on 2019/11/1. 
+ */
+public class newjianfa {
+
+    public static void main(String[] args) {
+        System.out.println("assasasaas");
+    }
+
+}
