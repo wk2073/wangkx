db0b79462403d556c86581d660eae84df50b7e27
wangkun <765505687@qq.com>
2019-11-01 17:27:37 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/jianfa1205/wangkxApp.java
@@ -0,0 +1,16 @@
+package com.atguigu.jianfa1205;
+
+/**
+ * @author by wangkun on 2019/11/1. 
+ */
+public class wangkxApp {
+
+    public static void main(String[] args) {
+        System.out.println("萨达萨达萨达撒");
+
+
+
+    }
+
+
+}
